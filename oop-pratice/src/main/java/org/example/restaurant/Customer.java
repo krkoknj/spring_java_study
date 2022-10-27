ea054f4015a7bc2d0be94f318f36d6395112771c
¤knj <krkoknj@gmail.com>
2022-10-27 16:22:35 +0900

    tdd, oop study

--- oop-pratice/src/main/java/org/example/restaurant/Customer.java
+++ oop-pratice/src/main/java/org/example/restaurant/Customer.java
@@ -1,4 +1,9 @@
 package org.example.restaurant;
 
 public class Customer {
+
+    public Cook order(String name, Menu menu, Cooking cooking) {
+        MenuItem menuItem = menu.choose(name);
+        return cooking.makeCook(menuItem);
+    }
 }
--- oop-pratice/src/test/java/org/example/restaurant/CustomerTest.java
+++ oop-pratice/src/test/java/org/example/restaurant/CustomerTest.java
@@ -3,6 +3,9 @@ package org.example.restaurant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -24,11 +27,13 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class CustomerTest {
     @Test
-    public void test1() {
-        //given
-        
-        //when
-        
-        //then
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다.")
+    public void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 8000)));
+        Cooking cooking = new Cooking();
+
+        assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
